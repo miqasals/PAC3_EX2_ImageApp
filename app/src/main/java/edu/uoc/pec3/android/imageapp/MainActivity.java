338027fd75692d970a8ce891bd8f5f7113f7b0ed
sgar810 <mgarcia@opentrends.net>
2016-03-21 18:06:21 +0100

    First commit

---
+++ app/src/main/java/edu/uoc/pec3/android/imageapp/MainActivity.java
@@ -0,0 +1,58 @@
+package edu.uoc.pec3.android.imageapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // TAG logs
+    private final String TAG = this.getClass().getSimpleName();
+
+    // Views
+    private Button mButtonOpenImage;
+    private ImageView mImageView;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set views
+        mButtonOpenImage = (Button) findViewById(R.id.button);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        // Set listeners
+        mButtonOpenImage.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mButtonOpenImage) {
+            // launching an intent to get an image from some gallery app
+        }
+    }
+}
